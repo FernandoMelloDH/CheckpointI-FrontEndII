03e97d96ec4dca647d37e11b2ecb6784d576d68c
FernandoMelloDH <famorim@digitalhouse.com>
2021-09-09 15:03:57 -0300

    Adição de Local Storage e JSON

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@ let elements = 0
 let urlImg = ""
 let title = ""
 let description = ""
-
-
+let json = {
+    'id': [],
+    'imagem': [],
+    'titulo': [],
+    'descricao': []
+}
 
 function createCard() {
     title = document.getElementById("title").value
@@ -16,4 +20,40 @@ function createCard() {
     cardSection.innerHTML += `<div> <img src="${urlImg}">
                                     <h2>${title}</h2>
                                     <p>${description}<p> </div>`;
+
+    //let json = `{"imagem":"${urlImg}", "titulo":"${title}"}`
+
+    //arrObj.push(urlImg)
+    //arrObj.push(title)
+    json.imagem.push(urlImg)
+    json.titulo.push(title)
+    json.descricao.push(description)
+    console.log(json)
+
+    //localStorage.setItem('imagem', urlImg)
+    //localStorage.setItem('titulo', title)
+
+    json = JSON.stringify(json) //Convertendo em JSON
+    console.log(json)
+    localStorage.setItem('json', json)
+    json = JSON.parse(json)
+}
+
+window.onload = function() {
+    //let imagem = localStorage.getItem('imagem')
+    //let titulo = localStorage.getItem('titulo')
+
+    let obj = localStorage.getItem('json')
+    obj = JSON.parse(obj)
+    console.log(obj)
+        //console.log(arr)
+
+    for (elements in obj.imagem) {
+        let cardSection = document.getElementById("card");
+        cardSection.innerHTML += `<div> <img src="${obj.imagem[elements]}"> 
+                                            <h2>${obj.titulo[elements]}</h2></div>`;
+
+        json.imagem.push(obj.imagem[elements])
+        json.titulo.push(obj.titulo[elements])
+    }
 }
